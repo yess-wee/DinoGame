abca6d14bd9181452c5fe3dc132bcb4e6f346aea
Yashvi Chauhan <yashvichauhan83@gmail.com>
2024-06-28 15:25:32 +0530

    completed implementation

--- app/src/main/java/com/yash/dinogame/MainActivity.java
+++ app/src/main/java/com/yash/dinogame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.yash.dinogame;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +11,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    WebView webView;
+    String url = "file:///android_asset/index.html";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        webView = findViewById(R.id.wv);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
     }
 }
